bd96244e1b788ecede01e79067957b33a9ce0fca
Shirshajit Sen Gupta <shirshajit@gmail.com>
2022-04-03 14:41:50 +0800

    HTTP Request package added

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,24 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    // simulate a network request for a username - async request
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return "Yoshi";
-    });
-    String bio = await Future.delayed(const Duration(seconds: 3), () {
-      return "vegan, musician, and egg collector.";
-    });
-    print("$username - $bio");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print("Hey there");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
